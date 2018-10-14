191cda3874bd27653a876ff1bdabe294741398de
Vinicios <vinicios@acmesecurity.org>
2018-10-14 17:03:40 -0300

    Arquivo Inicial

---
+++ main.c
@@ -0,0 +1,33 @@
+//
+//  Laboratory of Data Structure II
+//  Vinicios Barretos
+//  Vinicius Martins
+//  David Candelero
+//  Thiago Leal
+//
+
+/* Status
+ 0 -> New
+ 1 -> Ready
+ 2 -> In execution
+ 3 -> Blocked
+ 4 -> Done
+ */
+
+#include <stdio.h>
+
+typedef struct pcb {
+    struct pcb *next;
+    unsigned int id;
+    unsigned int quantum;
+    unsigned short int priority;
+    unsigned short int state;
+    unsigned int interruption;
+} pcb;
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
