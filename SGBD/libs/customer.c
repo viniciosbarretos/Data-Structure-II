abc0531fcb910d23165cbb194a3582ed3e3e72a0
David <davidcandelero@hotmail.com>
2018-12-08 19:23:36 -0200

    new structs and a example of hash function

--- SGBD/libs/customer.c
+++ SGBD/libs/customer.c
@@ -1,4 +1,14 @@
 #include "customer.h"
+#include <math.h>
+
+double hashFunction(unsigned accountNumber, unsigned globalDepth) {
+    int x = pow(2,globalDepth);
+    return (accountNumber % x);
+}
+
+void newBucket() {
+
+}
 
 void registerCustomer() {
 
--- SGBD/libs/customer.h
+++ SGBD/libs/customer.h
@@ -9,11 +9,26 @@ typedef struct customer {
     char *name;
     char customerType;
     unsigned overbalance;
-};
+} customer;
+
+typedef struct array[4] {
+    unsigned id;
+    customer *customerx;
+} array;
+
 typedef struct bucket {
+    struct array[4];
+} bucket;
 
-};
+typedef struct directory {
+    bucket *bucketx;
+} directory;
 
+double hashFunction(unsigned accountNumber, unsigned globalDepth);
+void newDirectory();
+void duplicateDirectory();
+void newBucket();
+void duplicateBucket();
 void registerCustomer();
 void removeCustomer();
 void searchCustomer();
