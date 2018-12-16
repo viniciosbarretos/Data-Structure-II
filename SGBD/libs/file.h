879a025db43af05a467fd86f1b477a631c4a5d6a
viniciusmartins98 <vinnyrp98@gmail.com>
2018-12-16 16:03:45 -0200

    Optimizing library "files".

--- SGBD/libs/file.h
+++ SGBD/libs/file.h
@@ -10,8 +10,5 @@
     int createFile (unsigned account_number, char *name, char customer_type, double overbalance);
     int lineCounter (unsigned accountNumber);
     void removeFileLine(int line);
-    double getCustomerOverBalancing (int line, int line_size);
-    char getCustomerType (int line, int line_size);
-    char * getCustomerName (int line, int line_size);
     Customer * getCustomer (int line, int line_size);
 #endif //SGBD_FILE_H
