1b26c35dbd537b1e1291da1a9e0627d4b67f901e
Thiago Pozati <thiago.pozati@unesp.br>
2018-12-16 20:06:17 -0200

    Improve render functions

--- SGBD/libs/render.c
+++ SGBD/libs/render.c
@@ -14,29 +14,24 @@ void clearScreen() {
 #endif
 }
 
-#define lineStart '-'
-#define lineH '-'
-#define lineV '|'
-#define lineJunction '+'
-#define lineEnd '>'
+typedef struct Line {
+    char lineStart;
+    char lineH;
+    char lineV;
+    char lineJunction;
+    char lineEnd;
+} Line;
+
+typedef struct Blackboard {
+    int width;
+    int height;
+    char **area;
+} Blackboard;
 
 typedef struct Trace {
     char direction;
     unsigned size;
 } Trace;
-
-//typedef struct
-
-// Clean a board.
-void zeroFill(char board[r][c], int row, int col) {
-    int i, j;
-    for (i=0; i<row; i++) {
-        for (j=0; j<col; j++) {
-            board[i][j] = ' ';
-        }
-    }
-}
-
 
 // Copy from input to buffer in a certain range.
 void strcpyfrgm(char *buffer, const char *input, int start, int size) {
@@ -54,7 +49,7 @@ void strcpyfrgm(char *buffer, const char *input, int start, int size) {
 
 // Split line trace string on a vector of Trace.
 int splitTrace(Trace traces[100], const char *string, char divider){
-    int i=0, j=0, lenght=0, start=0;
+    int i=0, j=0, length=0, start=0;
     int n = (int) strlen(string);
     char aux[100];
 
@@ -66,205 +61,268 @@ int splitTrace(Trace traces[100], const char *string, char divider){
     for (i=0; i<n; i++){
         if (string[i] == divider) {
             // Copy the fragment of trace.
-            strcpyfrgm(aux, string, start, lenght);
+            strcpyfrgm(aux, string, start, length);
             // Interpret and save the trace.
             sscanf(aux, "%c%d", &direction, &size);
             traces[j].direction = direction;
             traces[j].size = (unsigned) size;
             j++;
             // Reset markers.
-            lenght = 0;
+            length = 0;
             start=i+1;
         }
 
-        lenght++;
+        length++;
     }
 
     return j;
 }
 
-// Render a arrow line.
-void renderArrow(char board[r][c], int x, int y, int row, int col, const char *trace) {
-    int traceCount;
-    Trace traces[50];
-    char t = lineStart;
-    // Decompose the trace.
-    traceCount = splitTrace(traces, trace, ',');
-
-    // Render the start line
-    board[y][x] = t;
-    traces[0].size--;
-
-    // Render the trace.
-    for (int i=0; i<traceCount; i++) {
-        for (int j=0; j<traces[i].size; j++) {
-            // Go to next position.
-            switch (traces[i].direction) {
-                case 'R': x++; t=lineH; break;
-                case 'L': x--; t=lineH; break;
-                case 'D': y++; t=lineV; break;
-                case 'U': y--; t=lineV; break;
-                default:break;
-            }
-
-            // Check for junction.
-            if (j == traces[i].size-1) {
-                if (i != traceCount-1) {
-                    t = lineJunction;
-                } else {
-                    t = lineEnd;
-                }
-            }
-
-            // Render point on board.
-            board[y][x] = t;
-        }
-    }
-}
-
-
-//void copybetweenboards(char **buffer, char **input, int x, int y, );
-
-
-// Render the file fragment node.
-void renderFileDir(char board[r][c], int x, int y, int row, int col, Dir *dir) {
-    int a=6, b=4;
-    char aux[a][b];
-    int i, j;
-
-    //print globalDepth on aux board
-    sprintf(aux[0], "+++");
-    sprintf(aux[1], "+%d+", dir->globalDepth);
-    sprintf(aux[2], "+++");
-
-    a += 3;
-    b -= 2;
-
-    // Check if copy is possible.
-    if (a+y <= row && b + x <= col) {
-        // Copy for board.
-        for (i=0; i<a; i++) {
-            for (j=0; j<b; j++) {
-                board[y+i][x+j] = aux[i][j];
-            }
-        }
-    }
-
-    for (int k = 0; k < pow(2,dir->globalDepth); k++) {
-        //print dir on aux board
-        sprintf(aux[0], "+-----+");
-        sprintf(aux[1], "| %3d |", key);
-        sprintf(aux[2], "+-----+");
-
-
-        // Check if copy is possible.
-        if (a+y <= row && b + x <= col) {
-            // Copy for board.
-            for (i=0; i<a; i++) {
-                for (j=0; j<b; j++) {
-                    board[y+i][x+j] = aux[i][j];
-                }
-            }
-        }
-        a += 3;
-    }
-}
-
-
-void renderBucket(char board[r][c], int x, int y, int row, int col, int a, Bucket *bucket) {
-    int b=26;
-    char aux[a][b];
-    int i, j;
-    char k, z;
-
-    // Print on aux board.
-    sprintf(aux[0], "+Bucket %3d", key);
-    sprintf(aux[1], "+--------------------+ +-+");
-    sprintf(aux[2], "|%5d%5d%5d%5d| |%d|", bucket->items[0].id, bucket->items[1].id, bucket->items[2].id, bucket->items[3].id, bucket->localDepth);
-    sprintf(aux[3], "+--------------------++-+ ");
-
-    // Check if copy is possible.
-    if (a+y <= row && b + x <= col) {
-        // Copy for board.
-        for (i=0; i<a; i++) {
-            for (j=0; j<b; j++) {
-                k = aux[i][j];
-                z = board[y+i][x+j];
-                board[y+i][x+j] = aux[i][j];
-            }
-        }
-    }
-}
-
-void printBoard(char board[r][c], int row) {
+// Creates a new blackboard.
+Blackboard newBlackboard(int width, int height) {
     int i;
 
-    for (i=0; i<row; i++) {
-        printf("%s\n", board[i]);
+    // Initialize blackboard values.
+    Blackboard bb;
+    bb.width = width;
+    bb.height = height;
+
+    // Alloc memory for area.
+    bb.area = malloc(sizeof(char *) * height);
+    for (i=0; i < height; i++) {
+        bb.area[i] = malloc(sizeof(char) * width);
     }
 
+    return bb;
 }
 
-void cleanBoard(char board[r][c], int row, int col) {
+// Clean the area of the blackboard.
+void cleanBlackboard(Blackboard blackboard) {
+    int i, j;
+
+    for (i = 0; i < blackboard.height; i++) {
+        for (j = 0; j < blackboard.width; j++) {
+            blackboard.area[i][j] = ' ';
+        }
+    }
+}
+
+void startBlackboard(Blackboard blackboard) {
     int i;
 
     // Clean possible garbage on the board.
-    zeroFill(board, row, col);
+    cleanBlackboard(blackboard);
 
     // Add constraints to the borders.
-    for (i=0; i<row; i++) {
-        board[i][col-1] = '\0';
+    for (i = 0; i < blackboard.height; i++) {
+        blackboard.area[i][blackboard.width-1] = '\0';
     }
 }
 
-void printSequence(const char *str, int times) {
-    while ( times > 0 ) {
-        printf("%s", str);
-        times--;
-    }
-}
+// Copy the content from one blackboard to another.
+void copyBetweenBlackboards(Blackboard from, Blackboard to, int x, int y) {
+    int i, j;
 
-void showHashTable(Dir *dir, Bucket *bucket) {
+    // Check if copy is possible.
+    if (from.height+y <= to.height && from.width + x <= to.width) {
 
-    // Initiate control variables
-    int a = 1;
-    int aux = (int) pow(2,dir->globalDepth);
-    int aux2 = (8 + 12*(dir->globalDepth-2));
-    int count = 1;
-    int x = 0;
-    int y = 0;
-    char trace[20];
-
-    // Initiate the board.
-    char board[r][c];
-    cleanBoard(board, r, c);
-
-    renderFileDir(board, x, y, r, c, dir);
-
-    for (int i = 0; i < pow(2,dir->globalDepth); i++) {
-
-        if (dir->key != NULL) {
-            if (i < (pow(2,dir->globalDepth)/2)) {
-                sprintf(trace, "R%d,U%d,R%d,", (4*count), aux, aux2-(4*count)); //generic functions to render the arrow
-                renderArrow(board, x, y + 2, r, c, trace);
-                count++; //control variables of functions
-                aux -= 2;
-                renderBucket(board, x, y, r, c, a, bucket);
-                a +=5;
-            } else {
-                aux += 2; //control variables of functions
-                count--;
-                if () { //if 2 dirs point to the same bucket
-                    sprintf(trace, "R%d,D%d,R3,U19,R%d", (4*count), aux, aux2-(4*count)-2); //generic functions to render the arrow
-                    renderArrow(board, x, y + 2, r, c, trace);
-                } else {
-                    sprintf(trace, "R%d,U%d,R%d,", (4*count), aux, aux2-(4*count)); //generic functions to render the arrow
-                    renderArrow(board, x, y + 2, r, c, trace);
-                    renderBucket(board, x, y, r, c, a, bucket);
-                }
-                a +=5;
+        // Copy for board.
+        for (i = 0; i < from.height; i++) {
+            for (j = 0; j < from.width; j++) {
+                to.area[y+i][x+j] = from.area[i][j];
             }
         }
     }
-    printBoard(board, (int) (pow(2,dir->globalDepth)*4+(pow(2,dir->globalDepth)-1)));//altura maxima dir->globalDepth)*4+(pow(2,dir->globalDepth)-1)
+}
+
+// Print the content of the blackboard on terminal.
+void printBlackboard(Blackboard blackboard, int maxHeight) {
+    int i;
+
+    for (i = 0; i < maxHeight; i++) {
+        printf("%s\n", blackboard.area[i]);
+    }
+
+}
+
+// Render a arrow line on the blackboard.
+void renderArrow(Blackboard blackboard, int x, int y, const char *trace, Line line) {
+    int traceCount, i, j;
+    char t = line.lineStart;
+    Trace traces[50];
+
+    // Decompose the trace.
+    traceCount = splitTrace(traces, trace, ',');
+
+    // Render the first line part.
+    blackboard.area[y][x] = t;
+    traces[0].size--;
+
+    // Render the arrow, part after part.
+    for (i=0; i<traceCount; i++) {
+        for (j=0; j<traces[i].size; j++) {
+            // Go to next position.
+            switch (traces[i].direction) {
+                case 'R': x++; t=line.lineH; break;
+                case 'L': x--; t=line.lineH; break;
+                case 'D': y++; t=line.lineV; break;
+                case 'U': y--; t=line.lineV; break;
+                default: break;
+            }
+
+            // Check if its a junction.
+            if (j == traces[i].size-1) {
+                if (i != traceCount-1) {
+                    t = line.lineJunction;
+                } else {
+                    t = line.lineEnd;
+                }
+            }
+
+            // Render part on board with non destruction.
+            if (blackboard.area[y][x] == ' ') {
+                blackboard.area[y][x] = t;
+            }
+        }
+    }
+}
+
+// Render the SGBD hash directory in the blackboard.
+void renderFileDir(Blackboard blackboard, int x, int y, Dir *dir) {
+    int k, n;
+
+    // Initialize board.
+    Blackboard aux = newBlackboard(7, 3);
+    cleanBlackboard(aux);
+
+    // Calc the height of the dir.
+    n = (int) pow(2, dir->globalDepth);
+
+    // Print global depth on aux board.
+    sprintf(aux.area[0], "+---+  ");
+    sprintf(aux.area[1], "| %d |  ", dir->globalDepth);
+    sprintf(aux.area[2], "+---+  ");
+
+    // Copy for board.
+    copyBetweenBlackboards(aux, blackboard, x, y);
+
+    // Change insertion point.
+    y += aux.height-1;
+
+    // Render dir nodes.
+    for (k = 0; k < n; k++) {
+        // Print dir on aux board.
+        sprintf(aux.area[0], "+-----+");
+        sprintf(aux.area[1], "| %3d |", 3);
+        sprintf(aux.area[2], "+-----+");
+
+        // Copy for board.
+        copyBetweenBlackboards(aux, blackboard, x, y);
+
+        // Change insertion point.
+        y += aux.height-1;
+    }
+}
+
+void renderBucket(Blackboard blackboard, int x, int y, Bucket *bucket) {
+    Blackboard aux = newBlackboard(26, 5);
+
+    // Print on aux board.
+    sprintf(aux.area[0], "+Bucket %3d               ", 3);
+    sprintf(aux.area[1], "+---------------------++-+");
+    sprintf(aux.area[2], "| %4d %4d %4d %4d ||%d|", bucket->items[0].id, bucket->items[1].id, bucket->items[2].id, bucket->items[3].id, bucket->localDepth);
+    sprintf(aux.area[3], "| %4d %4d %4d %4d |+-+", bucket->items[0].line, bucket->items[1].line, bucket->items[2].line, bucket->items[3].line);
+    sprintf(aux.area[4], "+---------------------+   ");
+
+    // Copy for board
+    copyBetweenBlackboards(aux, blackboard, x, y);
+}
+
+void printConnectorArrow(Blackboard blackboard, int x, int y, int xEnd, int yEnd, int dist, Line line) {
+    char trace[20];
+    char direction;
+
+    if (y - yEnd > 0) {
+        direction = 'U';
+    } else {
+        direction = 'D';
+    }
+
+    sprintf(trace, "R%d,%c%d,R%d,", dist, direction, abs(y - yEnd), xEnd-x-dist);
+    renderArrow(blackboard, x, y, trace, line);
+}
+
+void printConnectorAroundArrow(Blackboard blackboard, int x, int y, int xEnd, int yEnd, int dist, int depth, Line line) {
+    char trace[20];
+
+    sprintf(trace, "R%d,D%d,R%d,U%d,L%d,", dist, depth - y + dist - 8, xEnd-x, depth - 8 + dist - yEnd, dist-2);
+    renderArrow(blackboard, x, y, trace, line);
+}
+
+void renderHashTable(Dir *dir) {
+    int i, x, y, n, dist, dirSize, yBucket, prev, depth;
+
+    // Calc the height of the dir.
+    n = (int) pow(2, dir->globalDepth);
+
+    // Initiate the board.
+    Blackboard blackboard;
+    blackboard = newBlackboard(120, 100);
+
+    // Clean the blackboard area and set end flag characters.
+    startBlackboard(blackboard);
+
+    // Create the line type.
+    Line line1 = {'-', '-', '|', '+', '>'};
+    Line line2 = {'~', '~', ':', '*', '<'};
+
+    // Get the number of different buckets in dir.
+    dirSize = differentBucketsInDir(dir);
+
+    // Calc the render position of dir.
+    x = 0;
+    y = ((dirSize * 6) - (dirSize * 2 + 6)) / 2;
+
+    // Render the dir.
+    renderFileDir(blackboard, x, y, dir);
+
+    // Start control variables.
+    depth = 0;
+    yBucket = 0;
+    y += 3;
+    dist = 10;
+
+    // Print the buckets
+    for (i = 0; i < n; i++) {
+        // Get the bucket previous position
+        prev = bucketAppearsBefore(dir, i);
+
+        // Check if connect to a new bucket or in a exist one.
+        if (prev == -1) {
+            // Render bucket and arrow.
+            renderBucket(blackboard, 31, yBucket, dir->key[i]);
+            printConnectorArrow(blackboard, 7, y, 30, yBucket+2, dist, line1);
+
+            // Increment yBucket counters.
+            yBucket += 6;
+
+        } else {
+            printConnectorAroundArrow(blackboard, 7, y, 56, 6 * prev + 2, dist, dirSize * 6, line2);
+            depth += 2;
+        }
+
+        // Alter dist.
+        if (i + 1 < n / 2) { // If its the first half.
+            dist += 3;
+        } else if (i >= n / 2 || n != dirSize){ // if its the last half + 1.
+            dist -= 3;
+        }
+
+        // Increase y counter.
+        y += 2;
+    }
+
+    depth += yBucket;
+
+    printBlackboard(blackboard, depth+3);
 }
--- SGBD/libs/render.h
+++ SGBD/libs/render.h
@@ -3,13 +3,8 @@
 
 #include "hashing.h"
 
-#define r 100
-#define c 120
 
-void cleanBoard(char board[r][c], int row, int col);
-void printBoard(char board[r][c], int row);
-void renderFileDir(char board[r][c], int x, int y, int row, int col, Dir *dir);
-void renderBucket(char board[r][c], int x, int y, int row, int col, int a, Bucket *bucket);
-void renderArrow(char board[r][c], int x, int y, int row, int col, const char *trace);
-void showHashTable(Dir *dir, Bucket *bucket);
+void renderHashTable(Dir *dir);
+
+
 #endif //SGBD_RENDER_H
