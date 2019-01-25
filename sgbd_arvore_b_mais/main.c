8f6e4ddc2ec4afb3ea5460b5e7e93663ee46bd2b
thiagoleal11 <thiagoleal11@gmail.com>
2019-01-25 15:46:36 -0200

    Some code to alter to new program. just ctrl+c and ctrl+v

--- sgbd_arvore_b_mais/main.c
+++ sgbd_arvore_b_mais/main.c
@@ -1,11 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "libs/render.h"
+#include "libs/bplustree.h"
+#include "libs/student.h"
+/*
+void addStudent(node *root, int defaultId) {
+
+    printHeader("Add a student");
+    Student student = getStudent();
+
+    // Get the costumer infos.
+
+    // Insert costumer in dir.
+    insertOnDir(dir, id, costumer);
+
+    // Show message
+    printHeader("File added!");
+}
+
+void deleteCustomer(Dir *dir) {
+    int id;
+    Item item;
+
+    printHeader("Remove a costumer");
+
+    // Get the costumer id.
+    printf ("\nType the Client Account Number: ");
+    scanf ("%d", &id);
+    cleanBuffer();
+
+    // Search for the item.
+    item = searchFromDir(dir, id);
+
+    // Remove or show error.
+    if (item.id != -1) {
+        removeFromDir(dir, id);
+        printHeader("Costumer removed");
+    } else {
+        printHeader("Costumer not found");
+    }
+}
+
+void searchCustomer(Dir *dir) {
+    int id;
+    Item item;
+    Customer customer;
+
+    printHeader("Search a costumer");
+
+    // Get the costumer id.
+    printf ("\nType the Client Account Number: ");
+    scanf ("%d", &id);
+    cleanBuffer();
+
+    // Search for the item.
+    item = searchFromDir(dir, id);
+
+    // Remove or show error.
+    if (item.id != -1) {
+        customer = readCustomer(item.line);
+        printCostumer(customer);
+    } else {
+        printHeader("Costumer not found");
+    }
+}
+
+// Menu options
+void printOptions() {
+    clearScreen();
+    printf("\n[1] - Register Customer\n");
+    printf("[2] - Remove Customer\n");
+    printf("[3] - Search Customer by Acc number\n");
+    printf("[4] - Show Hash Table\n");
+    printf("[0] - End Simulation\n");
+
+    printf("\nEnter with your option: ");
+}
+*/
+
+void readOrDefaultInt(const char *message, int defaultValue, int *result) {
+    char buffer[20];
+
+    printf("%s (%d):", message, defaultValue);
+    scanf("%s", buffer);
+}
 
 int main() {
-    printf("Hello world\n");
+    int result;
 
-    printRecordRow(3826, "Thiago Leal Pozati", "teste@unesp.br", 19, 1);
+//    readOrDefault("%d", &result, 15);
+
+    node * root = NULL;
+
+    Student s = {1, "tahio", "email", 19, 'a'};
+
+    for (int i = 0; i < 15; i++) {
+        root = insert(root, i, s);
+    }
+
+    print_tree(root);
+
+//    printf("Hello world\n");
+
+//    printRecordRow(3826, "Thiago Leal Pozati", "teste@unesp.br", 19, 1);
 
     return 0;
 }
