d63494d802358008202e52ab06693a94de626b54
thiagoleal11 <thiagoleal11@gmail.com>
2019-01-25 15:45:36 -0200

    Add students library

---
+++ sgbd_arvore_b_mais/libs/student.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "student.h"
+#include "utils.h"
+
+Student readStudent(int defaultID) {
+    // Initialize the costumer.
+    Student s;
+
+
+
+    // Get costumer information.
+    printf("\nName: ");
+    scanf("%[^\n]", s.name);
+    cleanBuffer();
+
+    printf("\nEmail: ");
+    scanf("%[^\n]", s.email);
+    cleanBuffer();
+
+    printf("\nAge: ");
+    s.age = getIntegerBetween(0, 200);
+
+    printf("\nType ['a' for active or 'f' for 'finalized']: ");
+    s.status = getCharInOptions("af", "Wrong value, try again.\nType ['a' for active or 'f' for finalized]: ");
+
+    return s;
+}
---
+++ sgbd_arvore_b_mais/libs/student.h
@@ -0,0 +1,12 @@
+#ifndef DATA_STRUCTURE_STUDENT_H
+#define DATA_STRUCTURE_STUDENT_H
+
+typedef struct Student {
+    int id;
+    char name[20];
+    char email[40];
+    int age;
+    char status;
+} Student;
+
+#endif //DATA_STRUCTURE_STUDENT_H
