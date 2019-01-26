31feabade73f3ede13d507f7dab561520ae773d0
DCandelero <davidcandelero557@gmail.com>
2019-01-26 04:00:54 +0000

    seems right

--- sgbd_arvore_b_mais/libs/student.c
+++ sgbd_arvore_b_mais/libs/student.c
@@ -6,6 +6,7 @@ Student getStudent(int defaultID) {
     // Initialize the costumer.
     Student s;
 
+    cleanBuffer();
     // Get costumer information.
     printf("\nName: ");
     scanf("%[^\n]", s.name);
@@ -17,6 +18,7 @@ Student getStudent(int defaultID) {
 
     printf("\nAge: ");
     s.age = getIntegerBetween(0, 200);
+    cleanBuffer();
 
     printf("\nType ['a' for active or 'f' for 'finalized']: ");
     s.status = getCharInOptions("af", "Wrong value, try again.\nType ['a' for active or 'f' for finalized]: ");
