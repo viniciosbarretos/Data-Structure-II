de4f639f619006a59506354fbad729c408d58b9b
thiagoleal11 <thiagoleal11@gmail.com>
2019-01-28 18:03:00 -0200

    Add function printMenu and printStudentBlock

--- sgbd_arvore_b_mais/libs/render.c
+++ sgbd_arvore_b_mais/libs/render.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "render.h"
 
+#define stdoutSize = 80;
+
 void printSequence(const char *str, int times) {
     while ( times > 0 ) {
         printf("%s", str);
@@ -37,6 +39,18 @@ void printStudentRow(int id, const char *name, const char *email, int age, char 
     printf("| %5d | %20s | %40s | %3d | %6c |\n", id, name, email, age, status);
 }
 
+void printStudentBlock(int id, const char *name, const char *email, int age, char status) {
+
+    printDivision("+", "-", 1, 50);
+    printf("| id:     %40d |\n", id);
+    printf("| Name:   %40s |\n", name);
+    printf("| Email:  %40s |\n", email);
+    printf("| Age:    %40d |\n", age);
+    printf("| Status: %40c |\n", status);
+    printDivision("+", "-", 1, 50);
+}
+
+// Todo better header.
 void printHeader(const char *str) {
     unsigned size = (unsigned) strlen(str);
     printf("\n");
@@ -44,4 +58,18 @@ void printHeader(const char *str) {
     printf("\n-   %s   -\n", str);
     printSequence("-", size + 8);
     printf("\n");
+}
+
+void printMenu() {
+    printf("\n");
+    printHeader("MENU");
+
+    printf("\n\nChoose an option entering the corresponding number:\n\n");
+    printf("[1] - Insert student record.\n");
+    printf("[2] - Delete student record.\n");
+    printf("[3] - Show student list.\n");
+    printf("[4] - 0.\n");
+    printf("[0] - Exit.\n\n");
+
+    printf("Option: ");
 }
--- sgbd_arvore_b_mais/libs/render.h
+++ sgbd_arvore_b_mais/libs/render.h
@@ -3,7 +3,9 @@
 
 void printHeader(const char *str);
 void printStudentRow(int id, const char *name, const char *email, int age, char status);
+void printStudentBlock(int id, const char *name, const char *email, int age, char status);
 void printStudentDivision();
 void printStudentRowHeader();
+void printMenu();
 
 #endif //SGBD_ARVORE_B_MAIS_RENDER_H
